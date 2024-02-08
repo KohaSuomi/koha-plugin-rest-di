577a17f1f5d2d9eab2c73b54bab9f651d7da2744
lmstrand <lmstrand@gmail.com>
2024-02-08 15:42:28 +0200

    Allow updating home library id and phone without modification request

--- Koha/Plugin/Fi/KohaSuomi/DI/PatronController.pm
+++ Koha/Plugin/Fi/KohaSuomi/DI/PatronController.pm
@@ -734,6 +734,8 @@ sub _parameters_require_modification_request {
         'privacy' => 1,
         'sms_number' => 1,
         'email' => 1,
+        'library_id' => 1,
+        'phone' => 1,
     };
 
     my $params = {
